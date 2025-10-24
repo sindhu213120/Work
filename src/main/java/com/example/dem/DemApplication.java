11c953b91731563390e3bb74d0d8e8702042c818
KSVNSINDHU <sindhupsvn13@gmail.com>
2025-10-23 18:12:52 -0600

    first

---
+++ src/main/java/com/example/dem/DemApplication.java
@@ -0,0 +1,14 @@
+package com.example.dem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemApplication.class, args);
+        System.out.println("Hello from IntelliJ!");
+	}
+
+}
