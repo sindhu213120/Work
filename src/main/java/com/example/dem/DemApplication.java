688e6c410bcc5c38a50dc2b3d41e5a205ef8cd76
KSVNSINDHU <sindhupsvn13@gmail.com>
2025-10-23 18:46:33 -0600

    Initial commit

--- src/main/java/com/example/dem/DemApplication.java
+++ src/main/java/com/example/dem/DemApplication.java
@@ -9,6 +9,7 @@ public class DemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemApplication.class, args);
         System.out.println("Hello from IntelliJ!");
+
 	}
 
 }
